5063fa298286e816066374450290df9a14f6836c
Mike Seiler <mikeseiler@Mikes-MacBook-Air.local>
2021-02-13 08:27:43 -1000

    checkin more code

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('jobrs.json');
+let rawdata = fs.readFileSync('tweet.json');
 let program = JSON.parse(rawdata);
 //console.log(JSON.stringify(program,null,4));
 
@@ -10,57 +10,55 @@ pragma solidity ^0.4.26;
 pragma experimental ABIEncoderV2;
 `
 for (const ContractName in contracts) {
-  const contract = contracts[ContractName]
-  const fields = contract.fields
+    const contract = contracts[ContractName]
+    const fields = contract.fields
 
-  template += `contract ${ContractName}{\n\n`
-  for (const field in contract.fields) {
-    const field_type = fields[field]
-    template += `\t${field_type} ${field};\n`
-  }
-
-  //now create init method
-
-  //define function name
-  template += `\n\tfunction ${ContractName}(`
-
-  //add all the pass-in methods
-  contract['initRules']['passIn'].forEach((element,index) => {
-    const type = fields[element]
-    template += `${type} _${element}`
-    if(index!=contract['initRules']['passIn'].length-1){
-      template += `, `
+    template += `contract ${ContractName}{\n\n`
+    for (const field in contract.fields) {
+        const field_type = fields[field]
+        template += `\t${field_type} ${field};\n`
     }
-  }
-  );
 
-  template += `){\n`
+    //now create init method
 
-  //create all the auto definitions
+    //define function name
+    template += `\n\tfunction ${ContractName}(`
 
-  for (const autofield in contract.initRules.auto) {
-    const value = contract.initRules.auto[autofield]
-    template += `\t\t${autofield} = ${value};\n`
-  }
+    //add all the pass-in methods
+    contract['initRules']['passIn'].forEach((element, index) => {
+        const type = fields[element]
+        template += `${type} _${element}`
+        if (index != contract['initRules']['passIn'].length - 1) {
+            template += `, `
+        }
+    });
 
-  //assign all the pass in variables using the _ hack
-  contract.initRules.passIn.forEach(autofield => {
-    template += `\t\t${autofield} = _${autofield};\n`
-  })
+    template += `){\n`
 
-  //end init
+    //create all the auto definitions
 
-  template += `\t}\n`
+    for (const autofield in contract.initRules.auto) {
+        const value = contract.initRules.auto[autofield]
+        template += `\t\t${autofield} = ${value};\n`
+    }
+
+    //assign all the pass in variables using the _ hack
+    contract.initRules.passIn.forEach(autofield => {
+        template += `\t\t${autofield} = _${autofield};\n`
+    })
+
+    //end init
+
+    template += `\t}\n`
 
 
-  //read rules
+    //read rules
 
-  contract.readRules.gets.forEach(field => {
-    const type = fields[field]
-    template += `\tfunction get_${field}() returns (${type}){return ${field};}\n`
-  }
-  );
-  template += `\n}`
+    contract.readRules.gets.forEach(field => {
+        const type = fields[field]
+        template += `\tfunction get_${field}() returns (${type}){return ${field};}\n`
+    });
+    template += `\n}`
 
 }
 
@@ -68,9 +66,9 @@ template += `
 contract App {\n`
 
 for (const ContractName in contracts) {
-  const contract = contracts[ContractName]
-  const fields = contract.fields
-  template += `
+    const contract = contracts[ContractName]
+    const fields = contract.fields
+    template += `
   address[] ${ContractName}_list; 
   uint256 ${ContractName}_list_length;
   event NewJobs(address creater); //todo loop and fill out fields
@@ -87,19 +85,46 @@ template += `
     bool exists;
     \n`
 for (const ContractName in contracts) {
-  template += `\taddress[] ${ContractName}_list; \n\tuint256 ${ContractName}_list_length;`
+    template += `\taddress[] ${ContractName}_list;
+  uint256 ${ContractName}_list_length;
+}`
 }
-template += `\n\t}\n\tmapping(address => UserInfo) public user_map;
-\taddress[] UserInfoList;
 
-function new_Jobs(string message_text) returns (address){
+
+
+template += `mapping(address => UserInfo) public user_map;
+\taddress[] UserInfoList;
+uint256 UserInfoListLength;
+`;
+
+
+for (const ContractName in contracts) {
+    const contract = contracts[ContractName]
+    const fields = contract.fields
+
+    template += `
+
+function new_${ContractName}(`
+
+
+    //add all the pass-in methods
+    contract['initRules']['passIn'].forEach((element, index) => {
+        const type = fields[element]
+        template += `${type} ${element}`
+        if (index != contract['initRules']['passIn'].length - 1) {
+            template += `, `
+        }
+    });
+
+    template += `)returns (address){\n
+  
+  
         
-  address message = address(new PublicMessage({_message:message_text, _sender:msg.sender }));
-  if(!txtrs[msg.sender].exists){
-      txtrs[msg.sender]=create_txtr_on_receive(msg.sender);
-      emit NewTxtr(msg.sender);
+  address mynew = address(new ${ContractName}({_text:text }));
+  if(!user_map[msg.sender].exists){
+    user_map[msg.sender]=create_txtr_on_receive(msg.sender);
   }
-  txtrs[msg.sender].all_public_messages.push(message);
+  user_map[msg.sender].all_public_messages.push(message);
   public_messages.push(message);
   public_messages_length+=1;
   emit NewPublicMessage(message, message_text,msg.sender);
@@ -113,6 +138,7 @@ function new_Jobs(string message_text) returns (address){
 }
 
 `
+}
 console.log(template)
 
 //todo create the parent contract and the events
