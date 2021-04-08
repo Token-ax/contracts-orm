36d90aa9b8d6f19ec278ca54e6f030a4d4c5d5aa
Mike Seiler <mikeseiler@Mikes-MacBook-Air.local>
2021-04-07 22:39:13 -1000

    build script

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
-node index.js > contracts/App.sol
-npx prettier --write 'contracts/App.sol'
+set -eux
+
+node index.js > tokenhost-web-template/contracts/App.sol
+npx prettier --write 'tokenhost-web-template/contracts/App.sol'
+#build site
+
+#rm -rf site
+cp -r tokenhost-web-template site
+(
+cd site
+yarn)
+
+
+node handlebar.js
+
+
+cd tokenhost-web-template/contracts; solcjs --bin --abi -o . App.sol; cp App_sol_App.abi App_sol_App.json
+
+
+node deploy.js
